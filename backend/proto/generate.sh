c8bf2b9e24a433f9fca0595bdd5bbea6e63c22b4
Vaibhav <166234139+reallvaibhav@users.noreply.github.com>
2025-05-06 00:34:16 +0500

    Delete backend directory

--- backend/proto/generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Generate Go code for statistics service
-protoc --go_out=. --go_opt=paths=source_relative \
-    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-    statistics.proto 
